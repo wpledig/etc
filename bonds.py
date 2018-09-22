3d446d843c0fef1b298a4a6ce07e729b0a5c270b
Joel <joelpabraham@gmail.com>
2018-09-22 13:06:46 -0500

    test

---
+++ bonds.py
@@ -0,0 +1,14 @@
+
+
+def trade_bonds(exchange, log, buy, sell):
+    if(len(buy) > 0 and buy[0][0] < 1000):
+        buy_best = buy[0]
+        price = buy_best[0]
+        size = min(buy_best[1], log.max_buy("BOND"))
+        return_val = add(exchange, random.randint(0, 2**32), "BOND", "BUY", price, size)
+
+    if(len(sell) > 0 and sell[0][0] > 1000):
+        sell_best = sell[0]
+        price = sell_best[0]
+        size = min(sell_best[1] if short else min(0, sell_best[0][1]), log.max_sell("BOND"))
+        return_val = add(exchange, random.randint(0, 2**32), "BOND", "SELL", price, size)
--- trade.py
+++ trade.py
@@ -13,6 +13,7 @@ import json
 import random
 import time
 import tracking
+import bonds
 
 
 log = tracking.TrackingBook()
@@ -80,23 +81,9 @@ def update_current_price(log, symbol, buy, sell):
 
 # ~~~~~============== TRADING  ==============~~~~~
 
-def random_id():
-    return random.randint(0, 2**32)
-
-def trade_bonds(exchange, buy, sell):
-    if(len(buy) > 0 and buy[0][0] < 1000):
-        buy_best = buy[0]
-        price = buy_best[0]
-        size = min(buy_best[1], log.max_buy("BOND"))
-        return_val = add(exchange, random_id(), "BOND", "BUY", price, size)
-
-    if(len(sell) > 0 and sell[0][0] > 1000):
-        sell_best = sell[0]
-        price = sell_best[0]
-        size = min(sell_best[1] if short else min(0, sell_best[0][1]), log.max_sell("BOND"))
-        return_val = add(exchange, random_id(), "BOND", "SELL", price, size)
 
 # ~~~~~============== MAIN LOOP ==============~~~~~
+
 
 def main():
     exchange = connect()
@@ -113,7 +100,7 @@ def main():
             buy = line['buy']
             sell = line['sell']
             if(symbol == "BOND"):
-                trade_bonds(exchange, buy, sell)
+                bonds.trade_bonds(exchange, log, buy, sell)
         if(log.book_dict["PNL"] != pnl):
             pnl = log.book_dict["PNL"]
             print(log.book_dict)
