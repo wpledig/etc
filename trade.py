5b6172b384232ca7f3d54ffee72d58991fd1d335
Joel <joelpabraham@gmail.com>
2018-09-22 12:14:29 -0500

    ok

--- trade.py
+++ trade.py
@@ -12,10 +12,10 @@ import socket
 import json
 import random
 import time
+import tracking
 
-# ~~~~~============== CONFIGURATION  ==============~~~~~
-# replace REPLACEME with your team name!
 
+log = tracking.TrackingBook()
 team_name = "Chasers"
 
 # This variable dictates whether or not the bot is connecting to the prod
@@ -28,9 +28,10 @@ test_mode = True
 # 1 is slower
 # 2 is empty
 
-positions = 0
+short = 1
 test_exchange_index = 0
 prod_exchange_hostname = "production"
+USDlimit = 30000
 
 port=25000 + (test_exchange_index if test_mode else 0)
 exchange_hostname = "test-exch-" + team_name if test_mode else prod_exchange_hostname
@@ -64,33 +65,40 @@ def cancel(exchange, id):
     write_to_exchange(exchange, {"type": "cancel", "order_id": id})
     return read_from_exchange(exchange)
 
+
+def update_current_price(log, symbol, buy, sell):
+    price = 0
+    if(len(buy) == 0 and len(sell) == 0):
+        return 0
+    elif(len(buy) == 0):
+        price = sell[0][0]
+    elif(len(sell) == 0):
+        price = buy[0][0]
+    else:
+        price = (buy[0][0] + sell[0][0])/2.0
+    log.update_price(symbol, price)
+
 # ~~~~~============== TRADING  ==============~~~~~
 
 def random_id():
     return random.randint(0, 2**32)
 
-def trade_bonds(exchange):
-    while True:
-        time.sleep(0.1)
-        return_val = " NONE "
-        line = read_from_exchange(exchange)
-        if(line['type'] == "book" and line['symbol'] == "BOND"):
-            buy = line['buy']
-            sell = line['sell']
-            if(len(buy) > 0 and buy[0][0] < 1000):
-                buy_best = buy[0]
-                price = buy_best[0]
-                size = buy_best[1]
-                return_val = add(exchange, random_id(), "BOND", "BUY", price, size)
-                print("\n\nOUTPUT: "+str(return_val)+"\n\n")
-            if(len(sell) > 0 and sell[0][0] > 1000):
-                sell_best = sell[0]
-                price = sell_best[0]
-                #size = min(positions, sell_best[0][1])
-                size = sell_best[1]
-                return_val = add(exchange, random_id(), "BOND", "SELL", price, size)
-		print("\n\nOUTPUT: "+str(return_val)+"\n\n")
-        #print(positions)
+def trade_bonds(exchange, buy, sell):
+    #time.sleep(0.1)
+    return_val = " NONE "
+    if(len(buy) > 0 and buy[0][0] < 1000):
+        buy_best = buy[0]
+        price = buy_best[0]
+        size = min(buy_best[1], log.max_buy("BOND"))
+        return_val = add(exchange, random_id(), "BOND", "BUY", price, size)
+        print("\n\nOUTPUT: "+str(return_val)+"\n\n")
+
+    if(len(sell) > 0 and sell[0][0] > 1000):
+        sell_best = sell[0]
+        price = sell_best[0]
+        size = min(sell_best[1] if short else min(0, sell_best[0][1]), log.max_sell("BOND"))
+        return_val = add(exchange, random_id(), "BOND", "SELL", price, size)
+        print("\n\nOUTPUT: "+str(return_val)+"\n\n")
 
 # ~~~~~============== MAIN LOOP ==============~~~~~
 
@@ -98,7 +106,19 @@ def main():
     exchange = connect()
     write_to_exchange(exchange, {"type": "hello", "team": team_name.upper()})
     hello_from_exchange = read_from_exchange(exchange)
-    trade_bonds(exchange)
+
+    while True:
+        line = read_from_exchange(exchange)
+        if(line['type'] == "fill"):
+            log.log(line['symbol'], line['dir'], line['price'], line['size'])
+        if(line['type'] == "book"):
+            symbol = line['symbol']
+            buy = line['buy']
+            sell = line['sell']
+            if(symbol == "BOND"):
+                trade_bonds(exchange, buy, sell)
+
+   
 # A common mistake people make is to call write_to_exchange() > 1
     # time for every read_from_exchange() response.
     # Since many write messages generate marketdata, this will cause an
