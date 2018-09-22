038ed9fb8a6c8d891a35509707f9d86b25c9a9f9
Joel <joelpabraham@gmail.com>
2018-09-22 17:15:09 -0500

    asdf

--- arbitrage.py
+++ arbitrage.py
@@ -1,14 +1,14 @@
 import random
 
 
-def arbitrage_adr(exchange, log, buy, sell, add):
+def arbitrage_adr(exchange, log, buy, sell, add, convert):
     if(len(sell) == 0):
         return
     fairval = log.price_dict["BABZ"]
     adr_val = sell[0][0]
     if(fairval > adr_val + 10):
         price = sell[0][0]
-        size = min(sell[0][1], log.max_buy("ADR"))
+        size = min(sell[0][1], log.max_buy("BABA"))
         add(exchange, random.randint(0, 2**32), "BABA", "BUY", price, size)
         convert(exchange, random.randint(0, 2**32), "BABA", "SELL", size)
         add(exchange, random.randint(0, 2**32), "BABZ", "SELL", fairval, size)
