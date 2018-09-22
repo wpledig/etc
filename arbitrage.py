90910fd8084fd3cefb11a825b13daef55a15f113
Joel <joelpabraham@gmail.com>
2018-09-22 17:17:53 -0500

    asdf

--- arbitrage.py
+++ arbitrage.py
@@ -9,6 +9,7 @@ def arbitrage_adr(exchange, log, buy, sell, add, convert):
     if(fairval > adr_val + 10):
         price = sell[0][0]
         size = min(sell[0][1], log.max_buy("BABA"))
+        print(log.max_buy("BABA"))
         add(exchange, random.randint(0, 2**32), "BABA", "BUY", price, size)
         convert(exchange, random.randint(0, 2**32), "BABA", "SELL", size)
         add(exchange, random.randint(0, 2**32), "BABZ", "SELL", fairval, size)
